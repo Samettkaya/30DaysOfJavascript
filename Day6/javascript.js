13c4c7eef807a06d7a61e00e2b435125bf7d17dc
samettkaya <kaya67380@gmail.com>
2022-02-07 13:55:27 +0300

    Day6

---
+++ Day6/javascript.js
@@ -0,0 +1,17 @@
+// Loops 
+
+// ---> for loop <---
+
+for (let i = 0; i <= 5; i++) {
+
+  console.log(i) // 0 1 2 3 4 5
+} 
+
+
+
+for (let i = 5; i >=0; i--) {
+
+  console.log(i) // 5 4 3 2 1 0
+} 
+
+ 
